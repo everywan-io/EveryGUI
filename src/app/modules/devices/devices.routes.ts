66f644d58ce0941c05bb1d52e36ed5ef1e420a79
Francesco Lombardo <franclombardo@gmail.com>
2020-03-14 22:28:10 +0100

    clean modules

--- src/app/modules/devices/devices.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListComponent } from '@modules/devices/components/list/list.component';
-
-
-export const DevicesRoutes: Routes = [{
-    path: '',
-    pathMatch: 'full',
-    component: ListComponent
-}];
--- src/app/modules/userslocation/userslocation.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListComponent } from '@modules/userslocation/components/list/list.component';
-
-
-export const UserslocationRoutes: Routes = [{
-    path: '',
-    pathMatch: 'full',
-    component: ListComponent
-}];
